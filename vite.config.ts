1794175b6bdafeb225455e348b047d4800d1fe47
ho-he-wa <tweivier@yahoo.co.jp>
2025-08-26 06:58:16 +0900

    fix: ビルド設定の誤り
    - jsのみdist/jsディレクトリ配下に生成される想定だったが実際にはmanifest.jsonや他のファイルも含めてdist/jsにコピーされる。distにmanifest.jsonがコピーされていない

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   build: {
-    outDir: "dist/js",
+    outDir: "dist",
     emptyOutDir: true, // distを毎回クリア
     rollupOptions: {
       input: {
-        content: resolve(__dirname, 'src/content.ts'),
-        popup: resolve(__dirname, 'src/popup.ts'),
-        background: resolve(__dirname, 'src/background.ts'),
-        dashboard: resolve(__dirname, 'src/dashboard.ts'),
+        content: resolve(__dirname, "src/content.ts"),
+        popup: resolve(__dirname, "src/popup.ts"),
+        background: resolve(__dirname, "src/background.ts"),
+        dashboard: resolve(__dirname, "src/dashboard.ts"),
       },
       output: {
-        entryFileNames: '[name].js',
+        entryFileNames: "[name].js",
       },
     },
   },
