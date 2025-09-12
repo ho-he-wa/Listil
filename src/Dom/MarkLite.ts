8c72e6ac275209119afddd969b4dcc8d0c913813
ho-he-wa <tweivier@yahoo.co.jp>
2025-09-12 18:10:54 +0900

    Refactor: RegExpの複製とフラグ設定

--- src/Dom/MarkLite.ts
+++ src/Dom/MarkLite.ts
@@ -36,9 +36,7 @@ export class MarkLite {
 
     // 2. 正規表現にマッチしたインデックスを探す
     const ranges: Range[] = [];
-    const regExpWk = regExp.flags.includes("g")
-      ? new RegExp(regExp.source, regExp.flags)
-      : new RegExp(regExp.source, regExp.flags + "g");
+    const regExpWk = new RegExp(regExp.source, addFlags(regExp.flags, "g"));
     regExpWk.lastIndex = 0;
     let match;
     while ((match = regExpWk.exec(fullText)) !== null) {
@@ -134,4 +132,15 @@ export class MarkLite {
       typeof Highlight !== "undefined"
     );
   }
+}
+/**
+ * 与えられた正規表現フラグ文字列に、追加のフラグを重複なく加える。
+ *
+ * @param originalFlags - 例: "i"
+ * @param flagsToAdd - 例: "g"
+ * @returns 新しいフラグ文字列。例: "gi"
+ */
+function addFlags(originalFlags: string, flagsToAdd: string): string {
+  const combined = new Set([...originalFlags, ...flagsToAdd]);
+  return [...combined].sort().join(""); // フラグ順は推奨順にソート
 }
