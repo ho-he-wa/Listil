268dcf45b12df7dc6c2e38f6704b5ee4b6e7d765
ho-he-wa <tweivier@yahoo.co.jp>
2025-09-13 18:48:42 +0900

    Change: AdvancedSettingsModalをドラッグ可能にする

--- src/content.ts
+++ src/content.ts
@@ -560,6 +560,31 @@ class AdvancedSettingsModal {
     this.currentKey = currentKey;
     this.modal = this.createModal();
     this.onchange = onchange;
+
+    const box = this.modal.querySelector<HTMLElement>(
+      '[data-name="listil-modal-content"]'
+    )!;
+    let isDragging = false;
+    let offsetX = 0;
+    let offsetY = 0;
+
+    box.addEventListener("mousedown", (e) => {
+      isDragging = true;
+      box.style.cursor = "grabbing";
+      offsetX = e.clientX - box.offsetLeft;
+      offsetY = e.clientY - box.offsetTop;
+    });
+
+    document.addEventListener("mousemove", (e) => {
+      if (!isDragging) return;
+      box.style.left = `${e.clientX - offsetX}px`;
+      box.style.top = `${e.clientY - offsetY}px`;
+    });
+
+    document.addEventListener("mouseup", () => {
+      isDragging = false;
+      box.style.cursor = "grab";
+    });
   }
 
   private currentSettingList(): FilterSettingList {
