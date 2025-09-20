c31aad8e5326d3f37a050629709fb5fdc64e522f
ho-he-wa <tweivier@yahoo.co.jp>
2025-09-20 18:57:36 +0900

    Change: redrawOfのデフォルト挙動をDOMの切り離しではなく非表示に変更
    - scriptを含む要素を切り離して再追加すると「Refused to load the script」エラーが発生するため

--- src/Dom/redrawOf.ts
+++ src/Dom/redrawOf.ts
@@ -2,17 +2,29 @@
  * DOM要素の再描画の回数を抑えるための関数
  *
  * - DOM 要素に対して複数の変更を行う（スタイル変更、属性の更新など）と、DOM に接続されているとブラウザが毎回再描画してしまうため、パフォーマンスが悪化する。
- *   この関数は、一旦DOMから取り外して変更し、最後に再表示することで、再描画の最小化を図るものである。
+ *   この関数は、一旦非表示あるいはDOMから切り離して変更し、最後に再表示することで、再描画の最小化を図るものである。
  * @param element 操作対象の要素
+ * @param options
  * @returns
  */
-export function redrawOf(element: HTMLElement) {
+export function redrawOf(
+  element: HTMLElement,
+  options: {
+    /**
+     * DOMから切り離すか否か。より高速であるがエラーに注意。
+     * 例えばscript要素を含む要素をDOMから切り離すと「Refused to load the script」エラーが発生する。
+     */
+    remove?: boolean;
+  } = {}
+) {
   const parent = element.parentNode;
   if (!parent) {
     throw new Error("操作対象の要素に親要素がありません。");
   }
   const nextSibling = element.nextSibling;
-  parent.removeChild(element);
+  options.remove && parent.removeChild(element);
+  element.classList.add("listil-force-hidden");
+
   const api: {
     edit: (callback: (el: HTMLElement) => void) => typeof api;
     show: () => void;
@@ -32,13 +44,16 @@ export function redrawOf(element: HTMLElement) {
      * 表示
      */
     show: (): void => {
-      if (parent.contains(element)) {
-        return;
-      }
-      if (nextSibling) {
-        parent.insertBefore(element, nextSibling);
-      } else {
-        parent.appendChild(element);
+      element.classList.remove("listil-force-hidden");
+      if (options.remove) {
+        if (parent.contains(element)) {
+          return;
+        }
+        if (nextSibling) {
+          parent.insertBefore(element, nextSibling);
+        } else {
+          parent.appendChild(element);
+        }
       }
       console.log("DEBUG", "再表示を行いました。");
     },
